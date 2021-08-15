da908a2335fb438a33d9a9d15f2e1a568c060b2f
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2021-08-15 20:18:05 +0530

    Add files via upload

---
+++ rnx-get/core/fil/ini_rnx_filler.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.awnto.rnx.core/files/usr/kbin/bash
+
+pdir=$HOME/.awnto
+pfol=rnx-d
+pfll=$pdir/$pfol
+
+mkdir -p $pfll
+
+cd $pdir
+
+if ! [ -f "${HOME}/.awnto/rnx-d/prock" ]
+then
+	rnx kill
+	rnx update
+	exec rnx login
+fi
+
+
