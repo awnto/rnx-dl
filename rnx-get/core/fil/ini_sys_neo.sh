29259765d3d5bc07f32a54b38d531beabd241cf4
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-06-26 10:48:50 +0530

    Update ini_sys_neo.sh

--- rnx-get/core/fil/ini_sys_neo.sh
+++ rnx-get/core/fil/ini_sys_neo.sh
@@ -52,7 +52,57 @@ ini_sys_finstall()
 	
 }
 ini_sys_finstall
+ini_sys_finstall_term()
+{
 
+	RNX_CORE_HOME=/data/data/com.awnto.rnx.core/files/home
+	RNX_CORE_UDOWN=/data/data/com.awnto.rnx.core/files/home/uDown
+	RNX_NEO_PREFIX=/data/data/com.awnto.rnx.core/files/neo/usr
+	RNX_CORE_PREFIX=/data/data/com.awnto.rnx.core/files/usr
+	
+	mkdir -p $RNX_NEO_PREFIX
+	
+	#chmod 700 ./assets/data/busybox
+	
+	mkdir -p $RNX_CORE_HOME
+	mkdir -p $RNX_NEO_PREFIX/kbin
+	
+	RNX_NEO_BUSYBOX=$RNX_NEO_PREFIX/kbin/busybox
+	ln -s -f -T ${NEO_AN_LIB}/liban-bi-bb.so $RNX_NEO_BUSYBOX
+	
+	
+	rnx_arch=`uname -m`
+
+	case $rnx_arch in 
+	
+	aarch64)
+	
+		echo CPU_ARCH_[AARCH64]_SUPPORTED > $RNX_CORE_HOME/rnx_tog.txt
+	
+		rnx_busybox=$RNX_NEO_PREFIX/kbin/busybox
+		
+		rnx_iusr_path="/sdcard/AWN/rnx_neo_usr_j_aarch64.tar.xz"
+		rnx_iusr_link="http://rnx-dl-4.awnto.com/neo/iusr/rnx_neo_usr_j_aarch64.tar.xz"
+
+
+		#exit 
+	;;
+
+
+	*)
+
+		echo CPU_ARCH_NOT_YET_SUPPORTED > $RNX_CORE_HOME/rnx_tog.txt
+		echo "arch : $rnx_arch not supported"
+
+		exit 12
+
+		;;
+
+	esac
+	
+	
+	
+}
 ini_sys_installer()
 {
 	# 1=name 2=folder 3=checkfile 4=offline_path 6=url
@@ -127,9 +177,18 @@ do
 	#echo "rechecking ..."
 	#sleep 1
 	#exec /data/data/com.awnto.rinix.io/files/assets/data/boot.sh
+ 
+ ini_sys_finstall
 	if ini_sys_installer iusr $RNX_CORE_PREFIX "prock" $rnx_iusr_path $rnx_iusr_link
 	then
 		(( ict = 1))
+  	else 
+   		ini_sys_finstall_term
+     		if ini_sys_installer iusr $RNX_CORE_PREFIX "prock" $rnx_iusr_path $rnx_iusr_link
+		then
+			(( ict = 1))
+  		fi
+     
 	fi
 
 done
