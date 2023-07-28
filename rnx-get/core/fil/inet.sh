568c2b1453fd362b07effe3e9bc2dc2fbb7d9164
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-07-28 19:01:27 +0530

    Update inet.sh

--- rnx-get/core/fil/inet.sh
+++ rnx-get/core/fil/inet.sh
@@ -21,7 +21,7 @@ rm work_inet
 #touch work_inet
 #wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
 
-rogg curl -L --output work_inet "http://v2vmqelrqj2np2d53dbw23hqzz2oonydjd7kkd4cuznlwkfjbwaoqzyd.onion.pet/web/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
+#rogg curl -L --output work_inet "http://v2vmqelrqj2np2d53dbw23hqzz2oonydjd7kkd4cuznlwkfjbwaoqzyd.onion.pet/web/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
 
 
 
