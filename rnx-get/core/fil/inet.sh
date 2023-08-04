808459bca961c2f843870c08b5ee71c66138065b
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-08-04 20:44:09 +0530

    Update inet.sh

--- rnx-get/core/fil/inet.sh
+++ rnx-get/core/fil/inet.sh
@@ -1,7 +1,11 @@
 #!/data/data/com.awnto.rnx.core/files/usr/kbin/bash
 
 
+roggg() {
 
+exec proot -b $PREFIX/rogg/etc:/etc "$@"
+
+}
 
 pdir=$HOME/.awnto
 pfol=rnx-d
@@ -21,8 +25,8 @@ rm work_inet
 #touch work_inet
 #wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
 
-rogg curl -L --output work_inet "http://tmpx.awnto.com:7080/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
-
+#rogg curl -L --output work_inet "http://tmpx.awnto.com:7080/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
+roggg curl -X POST -H "Content-Type: application/json" -d '{"this":"'"rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
 
 
 
