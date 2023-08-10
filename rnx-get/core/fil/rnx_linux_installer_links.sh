4b27eac08a271bd198926c8714ae03b9749cdf53
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-08-10 20:13:37 +0530

    Update rnx_linux_installer_links.sh

--- rnx-get/core/fil/rnx_linux_installer_links.sh
+++ rnx-get/core/fil/rnx_linux_installer_links.sh
@@ -4,3 +4,7 @@ debian_gui2 aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch6
 debian_gui3 aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
 debian_gui4 aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
 debian_gui5 aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
+
+
+
+
