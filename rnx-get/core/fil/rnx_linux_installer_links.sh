115709b4164c4eb6bf1d11b8a603e3fbdd9d869e
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-08-10 20:19:14 +0530

    Update rnx_linux_installer_links.sh

--- rnx-get/core/fil/rnx_linux_installer_links.sh
+++ rnx-get/core/fil/rnx_linux_installer_links.sh
@@ -1,6 +1,6 @@
 
 
-# name architecture lock_file local_path_for_offline_install web_URL_for_online_install
+# rootfs_name cpu_architecture lock_file local_path_for_offline_install web_URL_for_online_install
 
 debian_buster_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
 debian_gui1 aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
