bf289412ba7477ed589a13ba5792b85659604b01
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-08-10 19:48:12 +0530

    Update rnx_linux_installer_links.sh

--- rnx-get/core/fil/rnx_linux_installer_links.sh
+++ rnx-get/core/fil/rnx_linux_installer_links.sh
@@ -1,1 +1,6 @@
 debian_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
+debian_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
+debian_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
+debian_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
+debian_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
+debian_gui aarch64 awnto/prock /sdcard/AWN/rnx_debian_linux_stable_gui_j_aarch64.tar.xz https://gitlab.com/rishabhsarswa/rnx-dl-10/-/raw/main/rnx_debian_linux_stable_gui_j_aarch64.tar.xz
