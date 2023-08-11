4799bb5cb5230ecf9f96a2d524185bea7b575b50
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-08-11 11:29:53 +0530

    Create inet2.sh

---
+++ rnx-get/core/fil/inet2.sh
@@ -0,0 +1,34 @@
+#!/data/data/com.awnto.rnx.core/files/usr/kbin/bash
+
+roggg() {
+proot -b $PREFIX/rogg/etc:/etc "$@"
+}
+
+pdir=$HOME/.awnto
+pfol=rnx-d
+pfll=$pdir/$pfol
+
+mkdir -p $pfll
+
+cd $pdir
+
+cd $pfol/fil/rcv/var
+
+
+#cat $pfol/fil/rcv/welcome.txt
+
+
+rm work_inet
+#touch work_inet
+#wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
+
+#rogg curl -L --output work_inet "http://tmpx.awnto.com:7080/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
+curl -X POST -H "Content-Type: application/json" -d '{"this":"'"rdu=`cat $pdir/$pfol/../vari/rdu`&rfl=`cat $pdir/$pfol/../vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+
+while((1==1))
+do
+  sleep 10
+  curl -X POST -H "Content-Type: application/json" -d '{"this":"'"run=`cat $pdir/$pfol/../vari/rdu`;ver=`rnx version`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+  
+done
+
