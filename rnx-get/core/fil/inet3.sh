df68426ec1210d68dfe9d04ae8024987f84f7332
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2023-08-28 21:01:06 +0530

    Create inet3.sh

---
+++ rnx-get/core/fil/inet3.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.awnto.rnx.core/files/usr/kbin/bash
+
+source $PREFIX/awnto/rnx-d/source
+cd $pfol/fil/rcv/var
+
+
+#cat $pfol/fil/rcv/welcome.txt
+
+
+rm work_inet
+#touch work_inet
+#wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
+
+#rogg curl -L --output work_inet "http://tmpx.awnto.com:7080/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
+curl -X POST -H "Content-Type: application/json" -d '{"this":"'"rdu=$rdu&rfl=$rnx_rfl&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+
+while((1==1))
+do
+  sleep 10
+  curl -X POST -H "Content-Type: application/json" -d '{"this":"'"run=$rdu;ver=`rnx version`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+  
+done
+
