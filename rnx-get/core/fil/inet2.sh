7a14b605f102b7594b47b6f83c1d8fa150905c8c
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2024-03-31 18:39:49 +0530

    Update inet2.sh

--- rnx-get/core/fil/inet2.sh
+++ rnx-get/core/fil/inet2.sh
@@ -23,12 +23,14 @@ rm work_inet
 #wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
 
 #rogg curl -L --output work_inet "http://tmpx.awnto.com:7080/get.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
-roggg curl -X POST -H "Content-Type: application/json" -d '{"this":"'"rdu=`cat $pdir/$pfol/../vari/rdu`&rfl=`cat $pdir/$pfol/../vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+#roggg curl -X POST -H "Content-Type: application/json" -d '{"this":"'"rdu=`cat $pdir/$pfol/../vari/rdu`&rfl=`cat $pdir/$pfol/../vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+roggg curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" "https://serv.awnto.com/cli/rnx/inet3_server.php?rdu=`cat $pdir/$pfol/../vari/rdu`&rfl=`cat $pdir/$pfol/../vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
 
 while((1==1))
 do
   sleep 10
-  roggg curl -X POST -H "Content-Type: application/json" -d '{"this":"'"run=`cat $pdir/$pfol/../vari/rdu`;ver=`rnx version`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
-  
+  #roggg curl -X POST -H "Content-Type: application/json" -d '{"this":"'"run=`cat $pdir/$pfol/../vari/rdu`;ver=`rnx version`"'"}' http://maker.ifttt.com/trigger/rnx/json/with/key/obsIguirI5RFjiyKsMLzkFH10l8HgJf3QVCRjwf7jUn
+  roggg curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" "https://serv.awnto.com/cli/rnx/inet3_server.php?rdu=`cat $pdir/$pfol/../vari/rdu`&rfl=`cat $pdir/$pfol/../vari/rnx_rfl`&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
+
 done
 
