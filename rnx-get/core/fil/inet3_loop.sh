bf4bdbcc4e0bb0a91dbb38fdd9f0124015d493fa
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2024-03-31 18:05:45 +0530

    Update inet3_loop.sh

--- rnx-get/core/fil/inet3_loop.sh
+++ rnx-get/core/fil/inet3_loop.sh
@@ -4,3 +4,6 @@ source $PREFIX/awnto/rnx-d/source
 cd $pfol/fil/rcv/var
 
 echo "it is working"
+
+curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" "https://serv.awnto.com/cli/rnx/inet3_server.php?rdu=$rdu&rfl=$rnx_rfl&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`"
+
