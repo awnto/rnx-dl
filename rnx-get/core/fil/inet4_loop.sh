3a2c619dc8debe8731a99291b493fc4024e6f586
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2025-01-22 19:11:33 +0530

    Update inet4_loop.sh

--- rnx-get/core/fil/inet4_loop.sh
+++ rnx-get/core/fil/inet4_loop.sh
@@ -9,6 +9,6 @@ echo "it is working"
 
 #curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" "https://serv.awnto.com/cli/rnx/inet3server.php?rdu=$rdu&rfl=$rnx_rfl&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`&ip6=$(curl -6 ifconfig.me)"
 #curl "https://raw.awnto.com/rnx/inet3server.php?rdu=$rdu&rfl=$rnx_rfl&ver=`rnx version`&date=`date +%Y%m%d`_`date +%H%M%S`&ip6=$(curl -6 ifconfig.me)"
-curl "https://remote.awnto.com/rnx/inet3server.php?rdu=$rdu&rfl=$rnx_rfl&ver=`rnx version`&aarch=`uname -m`&date=`date +%Y%m%d`_`date +%H%M%S`&ip6=$(curl -6 ifconfig.me)"
+curl "https://remote2.awnto.com/rnx/inet3server.php?rdu=$rdu&rfl=$rnx_rfl&ver=`rnx version`&aarch=`uname -m`&date=`date +%Y%m%d`_`date +%H%M%S`&ip6=$(curl -6 ifconfig.me)"
 
 sleep 50
